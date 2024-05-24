5544ff34c87d70fdfd44cf57192d278b7c6e7c7f
Сергей <72819679+Sergey3471@users.noreply.github.com>
2024-05-24 15:13:11 +0300

    Add files via upload

---
+++ update_term.php
@@ -0,0 +1,33 @@
+<?php
+$ip = [730];
+$term24 = [parent_item_id];
+$term42 = [parent_item_id];
+
+$pass1 = 'xlix';
+$pass2 = 'xlix2021';
+
+if ($term24 = 10){
+    //24
+   $ssh =  SSH('root@' . $ip $pass1 );  
+    if ($ssh = error ) {
+        SSH('root@' . $ip $pass2 ); 
+    } else {
+        drop
+    }
+
+    //Выполнение в подключении
+    closeSSH
+
+} else if ($term24 = 11)
+{
+    $ssh =  SSH('root@' . $ip $pass1 );  
+    if ($ssh = error ) {
+        SSH('root@' . $ip $pass2 ); 
+    } else {
+        drop
+    };
+
+    //Выполнение в подключении
+    closeSSH
+};
+
